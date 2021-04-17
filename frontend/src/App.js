a7742de3b84a8ba55f16aad1d095a8fe600432fe
saa1605 <saaket.agashe@gmail.com>
2021-04-17 10:59:10 +0530

    React frontend initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from 'react';
+import UserText from './components/UserText';
+
+function App() {
+
+  return (
+    <div className="App">
+      <UserText></UserText>
+    </div>
+  );
+}
+
+export default App;
